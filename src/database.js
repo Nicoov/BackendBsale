ed98081602fb01854d1aba44b1f70d92cb735565
Nicoov <nicolas_ignacio_1@hotmail.com>
2022-09-04 14:53:32 -0300

    update Keep Alive

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-const configuration = ({
+const connection = mysql.createConnection({
     host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
     user: 'bsale_test',
     password: 'bsale_test',
@@ -9,37 +9,33 @@ const configuration = ({
 
 
 
+
+connection.connect((err) => {
+    if (err) {
+        res.status(500).send('ERROR');
+    } else {
+        console.log('bD CONNECTED');
+    }
+});
+
+
 //Solucion Keep Alive 
-// Dado que el servidor se cierra a los 5 seg que no existan peticiones 
-function startConnection() {
-    console.error('CONNECTING');
-    connection = mysql.createConnection(configuration);
-    connection.connect(function (err) {
-        if (err) {
-            console.error('CONNECT FAILED', err.code);
-            startConnection();
-        }
-        else
-            console.error('CONNECTED');
-    });
-    connection.on('error', function (err) {
-        if (err.fatal)
-            startConnection();
-    });
-}
-
-startConnection();
-
-
-// testing a select every 3 seconds :
-// to try the code you can stop mysql service => select will fail
-// if you start mysql service => connection will restart correctly => select will succeed
 setInterval(function () {
-    connection.query('select 1', function (err, results) {
-        if (err) console.log('SELECT', err.code);
-        else console.log('SELECT', results);
-    });
-}, 3000);
+    connection.query('SELECT 1')
+    console.log('KEEP ALIVE')
+}, 4500)
+
+
+
+// // testing a select every 3 seconds :
+// // to try the code you can stop mysql service => select will fail
+// // if you start mysql service => connection will restart correctly => select will succeed
+// setInterval(function () {
+//     connection.query('select 1', function (err, results) {
+//         if (err) console.log('SELECT', err.code);
+//         else console.log('SELECT', results);
+//     });
+// }, 3000);
 
 
 module.exports = connection;
